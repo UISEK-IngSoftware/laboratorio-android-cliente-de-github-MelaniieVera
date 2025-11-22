91ac14b504592b42eea1e1ee254b3ac2800c8751
melaniie97 <melaniievera.97@gmail.com>
2025-11-22 18:05:48 -0500

    Laboratorio 5 terminado

--- app/src/main/java/ec/edu/uisek/githubclient/MainActivity.kt
+++ app/src/main/java/ec/edu/uisek/githubclient/MainActivity.kt
@@ -1,14 +1,11 @@
 package ec.edu.uisek.githubclient
 
-import android.app.Activity
+
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import ec.edu.uisek.githubclient.databinding.ActivityMainBinding
 import ec.edu.uisek.githubclient.models.Repo
